3e7490c8b7406de3782e240408be846d1a304002
roobeen-b <rubinbaidhya@gmail.com>
2025-01-20 20:13:12 +0545

    typo correction

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -41,7 +41,7 @@ export const signUp = async (params: AuthCredentials) => {
     });
 
     await workflowClient.trigger({
-      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
       body: {
         email,
         fullName,
